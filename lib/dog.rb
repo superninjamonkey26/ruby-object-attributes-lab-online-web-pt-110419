461e989415b08bf45878b8ccfd09a22e808af6a3
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-24 02:57:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,4 @@
 class Dog
   def name=(dogs_name)
     @this_dogs_name = dogs_name
-  end
- # def name
-#    @this_dogs_name
- # end
 end
-fido = Dog.new
-fido.instance_variable_get(:@name)
-fido.instance_variable_set(:@name)
